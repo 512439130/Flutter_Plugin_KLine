4b9ed99d54eb4d777bb5e03209a0dd5550de3e60
SoMustYY <13160677911@163.com>
2021-08-17 12:40:34 +0800

    增加配置，解决Flutter嵌入NativeView滑动冲突。

--- lib/kline_widget.dart
+++ lib/kline_widget.dart
@@ -19,17 +19,24 @@ class _MyKLineState extends State<KLineWidget> {
   String viewType = "com.zb.flutter.kline/klineView";
   @override
   Widget build(BuildContext context) {
+
     print("refreshKLineView-theme:"+widget.theme.toString());
     var params = {
       'data': widget.data,
       "theme":widget.theme,
-    }; // 视图创建参数，可以被插件用来传递构造函数参数到嵌入式Android视图
+    };
+
     // 视图创建完毕的回调
     PlatformViewCreatedCallback callback = (id) {
       print("PlatformViewCreatedCallback:"+id.toString());
     };
-    // 判断设备类型，也可用：defaultTargetPlatform == TargetPlatform.android
-    Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers = <Factory<OneSequenceGestureRecognizer>>[Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer())].toSet();  //滑动冲突
+
+    /// 手势控制
+    Set<Factory<OneSequenceGestureRecognizer>> gestureRecognizers = [
+      Factory<HorizontalDragGestureRecognizer>(() => HorizontalDragGestureRecognizer()),
+      Factory<ScaleGestureRecognizer>(() => ScaleGestureRecognizer()),
+      Factory<LongPressGestureRecognizer>(() => LongPressGestureRecognizer())
+    ].toSet();
 
     if (Platform.isAndroid) {
       return AndroidView(
